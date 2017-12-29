72e142684f1fff30f1d967f3a8e6064274faa3f3
janice <janicejiang.d@gmail.com>
2017-12-29 23:22:41 +0800

    sign up successfully with flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,13 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-
+      flash[:success] = "Welcome to the Mini Weibo!"
+      redirect_to @user # user_url(@user)
     else
       render 'new'
     end
   end
-
+  
   def show
     @user = User.find(params[:id])
   end

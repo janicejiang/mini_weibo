3a4fd4b88db2c59c6996121bcf8a4be367fd93c3
janice <janicejiang.d@gmail.com>
2017-12-30 18:02:44 +0800

    add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
-  post 'signup', to: 'users#create'
-
   resources :users
+
+  get '/contact', to: 'static_pages#contact'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end

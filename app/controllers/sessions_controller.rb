becd8aae242c5e1c134f61583662e9528d45dc87
janice <janicejiang.d@gmail.com>
2018-01-01 20:14:15 +0800

    test remember me function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    @user = User.find_by(email: params[:session][:email].downcase)
 
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      remember(user) if params[:session][:remember_me] == '1'
-      redirect_to user
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      remember(@user) if params[:session][:remember_me] == '1'
+      redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'

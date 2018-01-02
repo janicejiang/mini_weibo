abd8cb8ffdaf2202850ce92ed849624c4add8cc1
janice <janicejiang.d@gmail.com>
2018-01-02 22:08:16 +0800

    must log in fisrt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -33,11 +35,19 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # 确保用户已登录
+    def logged_in_user
+      unless logged_in? # unless false
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end

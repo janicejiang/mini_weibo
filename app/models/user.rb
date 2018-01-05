0293b3e12aee31fcae839875f0c234f092f9177d
janice <janicejiang.d@gmail.com>
2018-01-05 22:03:40 +0800

    add activation to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-
-  # 在类中调用方法或访问属性时可以不用self, 赋值除外.
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -13,7 +12,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  
+
   # 返回指定字符串的哈希摘要
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -42,4 +41,16 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+    # 在类中调用方法或访问属性时可以不用self, 赋值除外.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # 创建并赋值激活令牌和摘要
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end

7f53fe2e9020a81ea5754142a096f2a7b327acd8
janice <janicejiang.d@gmail.com>
2018-01-08 20:44:33 +0800

    update the Users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,12 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Mini Weibo!"
-      redirect_to @user # user_url(@user)
+      # log_in @user
+      # flash[:success] = "Welcome to the Mini Weibo!"
+      # redirect_to @user # user_url(@user)
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account"
+      redirect_to root_url
     else
       render 'new'
     end

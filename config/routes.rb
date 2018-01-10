05508a525087788d32266e39f067eb110475234e
janice <janicejiang.d@gmail.com>
2018-01-10 17:45:19 +0800

    password_resets controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   get '/contact', to: 'static_pages#contact'
 

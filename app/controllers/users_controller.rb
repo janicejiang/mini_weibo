997d21c1183a7dcc9b3144dcf6ba22e28583976f
janice <janicejiang.d@gmail.com>
2018-01-16 17:37:28 +0800

    micropost access control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page], per_page: 9)
@@ -58,15 +58,6 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-    end
-
-    # 确保用户已登录
-    def logged_in_user
-      unless logged_in? # unless false
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
     end
 
     # 确保是正确的用户
